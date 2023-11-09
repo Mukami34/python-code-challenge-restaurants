516dc075c06dd1b9181e8cbdd1ce9bfae1a967dd
Mary <marymukami34@gmail.com>
2023-11-09 13:17:11 +0300

    Done

--- debug.py
+++ debug.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python3
 import random
+import ipdb;
+
+from lib.models import Review, Restaurant, Customer
+from lib.models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import ipdb;
+
 
 
 if __name__ == '__main__':
-    
+
     engine = create_engine('sqlite:///restaurants.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+    Base.metadata.create_all(engine)
+
+    
+    # Create instances of Restaurant, Customer, and Review
+    restaurant1 = Restaurant(name="Restaurant 1", price=2)
+    restaurant2 = Restaurant(name="Restaurant 2", price=3)
+
+    customer1 = Customer(first_name="John", last_name="Doe")
+    customer2 = Customer(first_name="Jane", last_name="Smith")
+
+    # Create reviews
+    review1 = Review(customer=customer1, restaurant=restaurant1, star_rating=4)
+    review2 = Review(customer=customer1, restaurant=restaurant2, star_rating=5)
+    review3 = Review(customer=customer2, restaurant=restaurant1, star_rating=3)
+
+    # Add the instances to the session and commit to the database
+    session.add(restaurant1)
+    session.add(restaurant2)
+    session.add(customer1)
+    session.add(customer2)
+    session.add(review1)
+    session.add(review2)
+    session.add(review3)
+    session.commit()
+
 
     ipdb.set_trace()
--- lib/models.py
+++ lib/models.py
@@ -1,19 +1,16 @@
 import os
 import sys
 
-sys.path.append(os.getcwd)
+sys.path.append(os.getcwd())
 
-from sqlalchemy import (create_engine, PrimaryKeyConstraint, Column, String, Integer)
-
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 engine = create_engine('sqlite:///db/restaurants.db', echo=True)
-
-
-# class Review(Base):
-#     pass
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
@@ -22,9 +19,12 @@ class Restaurant(Base):
     name = Column(String())
     price = Column(Integer)
 
+    # Define a one-to-many relationship between Restaurant and Review
+    reviews = relationship("Review", back_populates="restaurant")
+
     def __repr__(self):
         return f'Restaurant: {self.name}'
-
+    
 class Customer(Base):
     __tablename__ = 'customers'
 
@@ -32,5 +32,22 @@ class Customer(Base):
     first_name = Column(String())
     last_name = Column(String())
 
+    # Define a one-to-many relationship between Customer and Review
+    reviews = relationship("Review", back_populates="customer")
+
     def __repr__(self):
-        return f'Customer: {self.name}'
+        return f'Customer: {self.first_name} {self.last_name}'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+
+    # Define relationships
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    customer = relationship("Customer", back_populates="reviews")
+
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    restaurant = relationship("Restaurant", back_populates="reviews")
+
